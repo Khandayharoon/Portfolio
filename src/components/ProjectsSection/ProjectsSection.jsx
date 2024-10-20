bcd191ead8ed1e03130ae90a795ae962ec5c721d
khandayharoon <khandayharoon@gmail.com>
2024-10-20 20:55:28 +0530

    day20

--- src/components/ProjectsSection/ProjectsSection.jsx
+++ src/components/ProjectsSection/ProjectsSection.jsx
@@ -2,6 +2,7 @@ import { FaFreeCodeCamp } from "react-icons/fa";
 import { MdReplyAll } from "react-icons/md";
 import section1 from "../../assets/videos/section1.mp4";
 import exp1 from "../../assets/videos/exp1.mp4";
+import walking from "../../assets/videos/walking.mp4";
 import { useState } from "react";
 import Slideshow from "../Slideshow/Slideshow";
 function ProjectsSection() {
@@ -121,8 +122,119 @@ function ProjectsSection() {
         </div>
 
         <div className="w-full flex flex-wrap gap-4">
-          <div className="w-[500px] h-[600px] bg-red-300 rounded-2xl hover:backdrop-blur-lg shadow-lg"></div>
-          <div className="w-[900px] h-[600px] bg-red-800 rounded-2xl hover:backdrop-blur-lg shadow-lg"></div>
+          <div className="w-[500px] h-[600px] bg-red-300 rounded-2xl hover:backdrop-blur-lg shadow-lg">
+            <div className="hover:backdrop-blur-3xlw-[500px] h-[600px]  rounded-2xl">
+              <video
+                autoPlay
+                loop
+                muted
+                className=" w-full h-full object-cover rounded-2xl"
+              >
+                <source src={walking} type="video/mp4"></source>
+              </video>
+            </div>
+          </div>
+          <div className="w-[900px] h-[600px] bg-red-800 rounded-2xl hover:backdrop-blur-3xl shadow-lg">
+            {/* ghxfhxfjhfjhjhxfjhh */}
+            <div className="hover:backdrop-blur-3xl w-[900px] h-[600px]  rounded-2xl">
+              {/* <video
+                // autoPlay
+                // loop
+                // muted
+                className=" w-full h-full object-cover rounded-2xl"
+              >
+                <source src={exp1} type="video/mp4"></source>
+              </video> */}
+              <img
+                className="w-full h-full  rounded-2xl "
+                src="https://cdn.dribbble.com/userupload/14406138/file/original-392809e3cdb8768201afcea348ac3260.png?resize=1504x1128"
+                alt=""
+              />
+            </div>
+            <div
+              className="w-[900px] h-[600px] rounded-2xl  absolute top-0 hover:backdrop-blur-md "
+              onMouseEnter={() => setHoverCard("card4")}
+              onMouseLeave={() => setHoverCard(null)}
+            >
+              {/* Details about Porject                */}
+              {hoverCard === "card4" && (
+                <div className="z-50 text-white absolute bottom-4 mb-5 ml-5 cursor-pointer">
+                  <div>
+                    <div className="h-20 w-20">
+                      <img
+                        src="https://klindos.jzitnik.dev/favicon.png"
+                        alt=""
+                      />
+                    </div>
+                    <div>
+                      <h2 className="font-reader-regular text-8xl">
+                        PermissionLess
+                      </h2>
+                    </div>
+                    <div className="flex  gap-3">
+                      {/* skill 1 */}
+                      <div className="bg-zinc-100 w-36 h-14 rounded-xl py-1 px-3 flex justify-between items-center">
+                        <div className="h-full">
+                          <img
+                            className=" h-full"
+                            src="/icons/reactjs.svg"
+                            alt="React Icon"
+                          />
+                        </div>
+                        <div>
+                          <h1 className="text-black font-reader-regular">
+                            React_Js
+                          </h1>
+                        </div>
+                      </div>
+                      {/* skill 1 */}
+
+                      {/* skill 2 */}
+                      <div className="bg-zinc-100  h-14 rounded-xl  px-3 flex gap-1 justify-between items-center">
+                        <div className="h-full">
+                          <img
+                            className=" h-full"
+                            src="/icons/tailwindcss.svg"
+                            alt="Tailwindcss Icon"
+                          />
+                        </div>
+                        <div>
+                          <h1 className="text-black font-reader-regular">
+                            Tailwind CSS
+                          </h1>
+                        </div>
+                      </div>
+                      {/* skill 2 */}
+                      {/* skill 3 */}
+                      <div className="bg-zinc-100  h-14 rounded-xl  px-3 flex justify-between items-center">
+                        <div className="w-12 h-10">
+                          <img
+                            className=" w-12 h-10"
+                            src="/icons/framer-motion.svg"
+                            alt="Framer Motion Icon"
+                          />
+                        </div>
+                        <div>
+                          <h1 className="text-black font-reader-regular">
+                            Framer Motion
+                          </h1>
+                        </div>
+                      </div>
+                      {/* skill 3 */}
+                    </div>
+                    <div>
+                      <p>
+                        Designed and developed the frontend of a gadget
+                        e-commerce website <br /> with responsive UI and smooth
+                        interactions . . .
+                      </p>
+                    </div>
+                  </div>
+                </div>
+              )}
+            </div>
+            {/* ghsfhjhhsjhdj */}
+          </div>
         </div>
       </div>
       <div className="w-full h-[600px] bg-red-950 rounded-2xl hover:backdrop-blur-lg">
---
+++ src/components/Try/AnimatedSVG .jsx
@@ -0,0 +1,282 @@
+import "../../App.css"; // Import your CSS
+
+const AnimatedSVG = () => (
+  <div className="w-full h-full absolute -top-64 z-10">
+    <svg
+      viewBox="0 1 1000 1500"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+      className=""
+      stroke="currentColor"
+      strokeWidth="1"
+      strokeLinejoin="round"
+    >
+      <path
+        d="M191 356C191 456.453 480 466.601 480 682.5C480 855.22 480 1167.1 480 1376"
+        stroke="url(#gradient)"
+        strokeDasharray="250 200 150 400"
+        className="dash-animation-1"
+      />
+      <path
+        d="M501 454V1389.5"
+        stroke="url(#gradient)"
+        strokeDasharray="200 150 250 150"
+        className="dash-animation-2"
+      />
+      <path
+        d="M811 500.107C811 600.561 522 554.601 522 770.5C522 943.22 522 1176.1 522 1385"
+        stroke="url(#gradient)"
+        strokeDasharray="400 200 150 250"
+        className="dash-animation-3"
+      />
+      <defs>
+        <linearGradient
+          id="gradient"
+          x1="501.5"
+          y1="300"
+          x2="501.5"
+          y2="1400"
+          gradientUnits="userSpaceOnUse"
+        >
+          <stop stopOpacity="0" stopColor="currentColor" />
+          <stop offset="0.149999" stopColor="currentColor" />
+          <stop
+            offset="0.759999"
+            stopOpacity="0.335295"
+            stopColor="currentColor"
+          />
+          <stop offset="1" stopOpacity="0" stopColor="currentColor" />
+        </linearGradient>
+      </defs>
+    </svg>
+  </div>
+);
+
+export default AnimatedSVG;
+
+// import React from "react";
+
+// function Try() {
+//   return (
+//     <div className="w-full h-screen relative">
+//       <svg
+//         viewBox="0 0 1000 1500"
+//         fill="none"
+//         xmlns="http://www.w3.org/2000/svg"
+//         className="absolute inset-0 -z-10 hidden size-full  text-midnight/20 lg:block"
+//         stroke="currentColor"
+//         strokeWidth="1"
+//         strokeLinejoin="round"
+//       >
+//         <path
+//           d="M191 356C191 456.453 480 466.601 480 682.5C480 855.22 480 1167.1 480 1376"
+//           stroke="url(#gradient)"
+//           strokeDasharray="250 200 150 400"
+//           className="animate-[2s_dash_linear_reverse_infinite]"
+//         ></path>
+//         <path
+//           d="M501 454V1389.5"
+//           stroke="url(#gradient)"
+//           strokeDasharray="200 150 250 150"
+//           className="animate-[2s_dash_linear_0.5s_reverse_infinite]"
+//         ></path>
+//         <path
+//           d="M811 500.107C811 600.561 522 554.601 522 770.5C522 943.22 522 1176.1 522 1385"
+//           stroke="url(#gradient)"
+//           strokeDasharray="400 200 150 250"
+//           className="animate-[2s_dash_linear_0.75s_reverse_infinite]"
+//         ></path>
+//         <defs>
+//           <linearGradient
+//             id="gradient"
+//             x1="501.5"
+//             y1="300"
+//             x2="501.5"
+//             y2="1400"
+//             gradientUnits="userSpaceOnUse"
+//           >
+//             <stop stopOpacity="0" stopColor="currentColor"></stop>
+//             <stop offset="0.149999" stopColor="currentColor"></stop>
+//             <stop
+//               offset="0.759999"
+//               stopOpacity="0.335295"
+//               stopColor="currentColor"
+//             ></stop>
+//             <stop offset="1" stopOpacity="0" stopColor="currentColor"></stop>
+//           </linearGradient>
+//         </defs>
+//       </svg>
+//     </div>
+//   );
+// }
+
+// export default Try;
+
+// function Try() {
+//   return (
+//     <div>
+//       <section
+//         id="roadmap"
+//         className="container relative isolate grid scroll-mt-5 justify-center py-2-4"
+//       >
+//         <header className="grid max-w-prose gap-2 mx-auto max-w-prose justify-items-center text-center ">
+//           <p className="text-balance rounded-lg bg-white px-1 text-sm font-regular text-midnight">
+//             Let’s work together, wherever you are along the journey
+//           </p>{" "}
+//           <h2 className="text-xl text-balance">A Typical Project Roadmap</h2>{" "}
+//         </header>{" "}
+//         <ul className="mt-2-4 grid gap-9 sm:grid-cols-3 sm:items-start">
+//           <li className="grid justify-items-center gap-1 rounded-lg border border-white bg-gradient-to-b from-white to-white/0 p-2 text-center backdrop-blur-sm sm:[&amp;:nth-child(2)]:mt-2-4 sm:[&amp;:nth-child(3)]:mt-3-5">
+//             <svg className="aspect-square w-4">
+//               <use href="https://www.dinghy.studio/_app/immutable/assets/icons.BOZcFYiM.svg#sparkles"></use>
+//             </svg>{" "}
+//             <h3 className="font-regular">Greenfield Idea</h3>{" "}
+//             <p className="text-xs">
+//               Youre at the very beginning of something new and need to validate
+//               important assumptions towards target groups, conversion funnels
+//               and business goals.
+//             </p>{" "}
+//           </li>
+//           <li className="grid justify-items-center gap-1 rounded-lg border border-white bg-gradient-to-b from-white to-white/0 p-2 text-center backdrop-blur-sm sm:[&amp;:nth-child(2)]:mt-2-4 sm:[&amp;:nth-child(3)]:mt-3-5">
+//             <svg className="aspect-square w-4">
+//               <use href="https://www.dinghy.studio/_app/immutable/assets/icons.BOZcFYiM.svg#exclude-square"></use>
+//             </svg>{" "}
+//             <h3 className="font-regular">Existing Concept</h3>{" "}
+//             <p className="text-xs">
+//               Youre past the green field and have a wireframe or a design of a
+//               new feature, landing page, etc and would like to validate that you
+//               thought of everything and that users will understand how to use
+//               it.
+//             </p>{" "}
+//           </li>
+//           <li className="grid justify-items-center gap-1 rounded-lg border border-white bg-gradient-to-b from-white to-white/0 p-2 text-center backdrop-blur-sm sm:[&amp;:nth-child(2)]:mt-2-4 sm:[&amp;:nth-child(3)]:mt-3-5">
+//             <svg className="aspect-square w-4">
+//               <use href="https://www.dinghy.studio/_app/immutable/assets/icons.BOZcFYiM.svg#arrow-split"></use>
+//             </svg>{" "}
+//             <h3 className="font-regular">Revalidation of existing product</h3>{" "}
+//             <p className="text-xs">
+//               Youre running an established app or website and know that youd
+//               like to improve on that but dont know exactly where to start or
+//               what to prioritize.
+//             </p>{" "}
+//           </li>
+//         </ul>{" "}
+//         <ul className="relative mt-3-5 grid max-w-screen-xs gap-11 justify-self-center">
+//           <li className="grid gap-1 rounded-lg bg-white/50 p-2 shadow-lg backdrop-blur sm:flex">
+//             <svg className="aspect-square w-4 shrink-0">
+//               <use href="https://www.dinghy.studio/_app/immutable/assets/icons.BOZcFYiM.svg#sparkles"></use>
+//             </svg>{" "}
+//             <div className="flow">
+//               <h3 className="font-regular">Tell us your idea</h3>{" "}
+//               <p className="text-xs">
+//                 We start off with an inital consultation where we hear about
+//                 your idea, we gather your business goals and learn more about
+//                 your target audience
+//               </p>
+//             </div>{" "}
+//           </li>
+//           <li className="grid gap-1 rounded-lg bg-white/50 p-2 shadow-lg backdrop-blur sm:flex">
+//             <svg className="aspect-square w-4 shrink-0">
+//               <use href="https://www.dinghy.studio/_app/immutable/assets/icons.BOZcFYiM.svg#cube-with-dot"></use>
+//             </svg>{" "}
+//             <div className="flow">
+//               <h3 className="font-regular">Planning the project</h3>{" "}
+//               <p className="text-xs">
+//                 We prepare a project roadmap spanning over the course of three
+//                 weeks, we make sure to include sensible milestones, possible
+//                 difficulties and expected deliverables
+//               </p>
+//             </div>{" "}
+//           </li>
+//           <li className="grid gap-1 rounded-lg bg-white/50 p-2 shadow-lg backdrop-blur sm:flex">
+//             <svg className="aspect-square w-4 shrink-0">
+//               <use href="https://www.dinghy.studio/_app/immutable/assets/icons.BOZcFYiM.svg#headset"></use>
+//             </svg>{" "}
+//             <div className="flow">
+//               <h3 className="font-regular">Regular updates and Feedback</h3>{" "}
+//               <p className="text-xs">
+//                 Weekly touchpoints for project updates and feedback, ensuring
+//                 alignment at each phase.
+//               </p>
+//             </div>{" "}
+//           </li>
+//           <li className="grid gap-1 rounded-lg bg-white/50 p-2 shadow-lg backdrop-blur sm:flex">
+//             <svg className="aspect-square w-4 shrink-0">
+//               <use href="https://www.dinghy.studio/_app/immutable/assets/icons.BOZcFYiM.svg#repeat-once"></use>
+//             </svg>{" "}
+//             <div className="flow">
+//               <h3 className="font-regular">Iterative Sprints</h3>{" "}
+//               <p className="text-xs">
+//                 Structured review sessions with all stakeholders after each
+//                 milestone to incorporate feedback.
+//               </p>
+//             </div>{" "}
+//           </li>
+//           <li className="grid gap-1 rounded-lg bg-white/50 p-2 shadow-lg backdrop-blur sm:flex">
+//             <svg className="aspect-square w-4 shrink-0">
+//               <use href="https://www.dinghy.studio/_app/immutable/assets/icons.BOZcFYiM.svg#handshake"></use>
+//             </svg>{" "}
+//             <div className="flow">
+//               <h3 className="font-regular">
+//                 Final presentation &amp; Handover
+//               </h3>{" "}
+//               <p className="text-xs">
+//                 A conclusive presentation of all deliverables, followed by a
+//                 handover process.
+//               </p>
+//             </div>{" "}
+//           </li>
+//         </ul>{" "}
+//         <svg
+//           viewBox="0 0 1000 1500"
+//           fill="none"
+//           xmlns="http://www.w3.org/2000/svg"
+//           className="absolute inset-0 -z-10 hidden size-full  text-midnight/20 lg:block"
+//           stroke="currentColor"
+//           strokeWidth="1"
+//           strokeLinejoin="round"
+//         >
+//           <path
+//             d="M191 356C191 456.453 480 466.601 480 682.5C480 855.22 480 1167.1 480 1376"
+//             stroke="url(#gradient)"
+//             strokeDasharray="250 200 150 400"
+//             className="animate-[2s_dash_linear_reverse_infinite]"
+//           ></path>
+//           <path
+//             d="M501 454V1389.5"
+//             stroke="url(#gradient)"
+//             strokeDasharray="200 150 250 150"
+//             className="animate-[2s_dash_linear_0.5s_reverse_infinite]"
+//           ></path>
+//           <path
+//             d="M811 500.107C811 600.561 522 554.601 522 770.5C522 943.22 522 1176.1 522 1385"
+//             stroke="url(#gradient)"
+//             strokeDasharray="400 200 150 250"
+//             className="animate-[2s_dash_linear_0.75s_reverse_infinite]"
+//           ></path>
+//           <defs>
+//             <linearGradient
+//               id="gradient"
+//               x1="501.5"
+//               y1="300"
+//               x2="501.5"
+//               y2="1400"
+//               gradientUnits="userSpaceOnUse"
+//             >
+//               <stop stopOpacity="0" stopColor="currentColor"></stop>
+//               <stop offset="0.149999" stopColor="currentColor"></stop>
+//               <stop
+//                 offset="0.759999"
+//                 stopOpacity="0.335295"
+//                 stopColor="currentColor"
+//               ></stop>
+//               <stop offset="1" stopOpacity="0" stopColor="currentColor"></stop>
+//             </linearGradient>
+//           </defs>
+//         </svg>
+//       </section>
+//     </div>
+//   );
+// }
+
+// export default Try;
